230d913c7e7d517846151229379c685c961fa472
Alex Kritikos <alex@8bitb.us>
2012-02-24 17:45:16 -0600

    Support whatever paramaterization the backend uses

--- horrorm.py
+++ horrorm.py
@@ -18,6 +18,9 @@ class Field(object):
     def __init__(self, field_name):
         self.field_name = field_name
 
+    def sql(self, param):
+        return str(self)
+
     def __str__(self):
         return self.field_name
 
@@ -72,13 +75,12 @@ class Where(object):
     def __or__(self, rhs):
         return Where(self, 'OR', rhs)
 
-    def __str__(self):
+    def sql(self, param):
         if isinstance(self.rhs, Where):
-            rhs_str = str(self.rhs)
+            rhs_str = self.rhs.sql(param)
         else:
-            # TODO determine whether we should bind for postgresql or sqlite
-            rhs_str = '%s'
-        return '(%s %s %s)' % (str(self.lhs), self.connector, rhs_str)
+            rhs_str = param
+        return '(%s %s %s)' % (self.lhs.sql(param), self.connector, rhs_str)
 
     def params(self):
         if isinstance(self.lhs, Where):
@@ -104,8 +106,7 @@ class D(object):
 
     def __init__(self, *args, **kwargs):
         self.con = connect(*args, **kwargs)
-
-    # Should we use slots so tab-completion works?
+        # Should we create attributes here so tab-completion works in IPython?
 
     def __getattr__(self, table_name):
         return T(self.con, table_name)
@@ -118,6 +119,7 @@ class T(object):
     
     def __init__(self, con, *table_names):
         self.con = con
+        self.param = self.con.param
         self.table_names = table_names
 
     def __mul__(self, rhs):
@@ -132,7 +134,7 @@ class T(object):
         where = None
         for elm in args:
             if isinstance(elm, Field):
-                fields.append(str(elm))
+                fields.append(elm.sql(self.param))
             elif isinstance(elm, Where):
                 where = elm
         joined_fields = ', '.join(fields) or '*'
@@ -140,17 +142,21 @@ class T(object):
         if where is None:
             return self.con('SELECT %s FROM %s' % (joined_fields, self._joined_tables()))
         else:
-            return self.con('SELECT %s FROM %s WHERE %s' % (joined_fields, self._joined_tables(), str(where)), *where.params())
+            return self.con('SELECT %s FROM %s WHERE %s' % (joined_fields, self._joined_tables(), where.sql(self.param)), *where.params())
 
     def update(self, *args, **kwargs):
         # TODO: I have no idea what a nice API for this would be.
+        # update(fields=[id, name], values=[5, 'alex'], where)
+        # update(dict or namedtuple, where)
         pass
 
     def insert(self, *args, **kwargs):
         # TODO: I have no idea what a nice API for this would be.
+        # insert(fields=[id, name], values=[5, 'alex'])
+        # insert(dict or namedtuple, where)
         pass
 
     def delete(self, where):
         # This will error if you've specified multiple tables, but that's
         # probably better than blithely just deleting from the first one.
-        return self.con('DELETE FROM %s WHERE %s' % (self.joined_tables(), str(where)), *where.params())
+        return self.con('DELETE FROM %s WHERE %s' % (self.joined_tables(), where.sql(self.param)), *where.params())
